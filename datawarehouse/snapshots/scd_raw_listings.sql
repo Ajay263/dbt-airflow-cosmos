505a308393ec398244a05803df2184015b512a1a
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-12-18 19:08:44 +0000

    update

--- datawarehouse/snapshots/scd_raw_listings.sql
+++ datawarehouse/snapshots/scd_raw_listings.sql
@@ -1,12 +1,19 @@
 {% snapshot scd_raw_listings %}
+
 {{
- config(
- target_schema='prod',
- unique_key='id',
- strategy='timestamp',
- updated_at='updated_at',
- invalidate_hard_deletes=True
- )
+   config(
+       target_schema='prod',
+       unique_key='id',
+       strategy='timestamp',
+       updated_at='updated_at',
+       invalidate_hard_deletes=True
+   )
 }}
+
 select * FROM {{ source('airbnb_datawarehouse', 'listings') }}
+
 {% endsnapshot %}
+
+
+
+
